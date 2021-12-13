c13eab47d6c9380dc043b393f457fbbc735c019e
Guilherme Parente <guilherme.parente17@outlook.com>
2021-12-13 19:11:05 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="frame">
+      <div className="center">
+        <div className="number">
+          <div className="one-one"></div>
+          <div className="one-two"></div>
+          <div className="zero-one"></div>
+          <div className="zero-two"></div>
+        </div>
+        <span className="big">Days</span>
+        <span className="small">CSS Challenge</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
